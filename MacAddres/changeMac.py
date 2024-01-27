2959e7aedbcaaddfbca086fd144cd6c973210d59
Gustavo Henrique <ymooncake@gmail.com>
2024-01-27 17:51:34 -0300

    Update changeMac.py

--- MacAddres/changeMac.py
+++ MacAddres/changeMac.py
@@ -2,15 +2,19 @@ import subprocess
 import optparse
 
 parser = optparse.OptionParser()
+
+def changeMac(interface, newMac):
+    print("[+] Mudando o endereço mac de: "+interface+"para: "+newMac)
+
+    subprocess.run("ifconfig "+interface+" down") 
+    subprocess.run("ifconfig " +interface+" hw ether"+newMac)
+    subprocess.run("ifconfig "+interface+" up")
+
+
+#adicionando comandos por terminal
 parser.add_option("-i","--interface", dest = "interface", help = "Interface a mudar o MAC")
 parser.add_option("-m","--mac", dest = "newMac", help = "Endereço Mac a ser mudado")
 (options,arguments) = parser.parse_args()
 
-interface = options.interface
-newMac = options.newMac
+changeMac(options.interface, options.newMac)
 
-print("[+] Mudando o endereço mac de: "+interface+"para: "+newMac)
-
-subprocess.run("ifconfig "+interface+" down") 
-subprocess.run("ifconfig " +interface+" hw ether"+newMac)
-subprocess.run("ifconfig "+interface+" up")
