2f967956dce742efb8225347c18e3aae235f6d0c
Gustavo Henrique <ymooncake@gmail.com>
2026-09-16 13:17:54 -0300

    Update haknox.sh

--- PortScan/haknox.sh
+++ PortScan/haknox.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python3<<EOF
+python3 - "$@" <<EOF
 import socket
+import argparse
 
 ART="""\n
 
@@ -22,13 +23,29 @@ ART="""\n
 
 def main():
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-t" ,required=True)
+
+    args = parser.parse_args()
+
+    portas = [80,22]
+
+
+
     print(ART)
-    
-    try:
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
-            sock.settime(3)
-    except:
-        pass
+    for porta in portas:
+        try:
+            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+                sock.settimeout(3)
+                
+                result = sock.connect_ex((args.t,porta))
+
+                if result == 0:
+                    print(f"[+]{porta} aberta")
+
+        except socket.error as erro:
+            print(f"[!] Erro na porta {porta}: {erro}")
 
 if __name__ == '__main__':
     main()
